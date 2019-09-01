c8c10b70727d45e8291b49fc20bc0df5c7206a74
Will Jeremijenko <45901181+Veloc1tyE@users.noreply.github.com>
2019-09-01 17:46:31 +1000

    Update numbers.py

--- numbers.py
+++ numbers.py
@@ -7,15 +7,20 @@ Created on Wed Jul  3 22:38:07 2019
 
 import math
 def Triangle(n):
+    # Creates the nth Triangular number
     return n*(n+2)/2
 
 def Pentagon(n):
+    # Creates the nth Pentagonal number
     return n*(3*n-1)/2
 
 def Hexagon(n):
+    # Creates the nth Hexagonal number
     return n*(2*n-1)
 
 def Solver(a, b, c):
+    """Solves the Diophantine equation
+       """
     D = b**2 - 4*a*c
     if D < 0:
         return None
@@ -29,6 +34,7 @@ def Solver(a, b, c):
 matcher = 0
 n = 286
 while True:
+    # Continues to iterate until it finds a number that is triangular, pentagonal and hexagonal
     const = Triangle(n)
     if int(Pentagon(Solver(1.5, -0.5, -const))) == int(const):
         if int(Hexagon(Solver(2, -1, -const))) == int(const):
