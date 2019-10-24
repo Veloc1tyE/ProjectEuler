4765176e98aa60ae4c5f86554ad2e362ecfc6974
Will Jeremijenko <45901181+Veloc1tyE@users.noreply.github.com>
2019-10-25 09:32:55 +1000

    Create p2.swift

---
+++ Swift/p2.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+let tests = Int(readLine()!)
+
+for i in 1...tests! {
+    let limit = Int(readLine()!)
+
+    var prev = 0
+    var next = 1
+    var num = 0
+    var sum = 0
+
+    while next < limit! {
+        /*
+        Sequentially compute the fibonacci sequence
+        up to a certain limit, then add on to running
+        total if even
+        */
+        num = prev
+        prev = next
+        next += num
+        if (prev % 2 == 0) {
+            sum += prev
+        }
+    }
+    // And this will be the sum of all even fibonacci's
+    // Below this point
+    print(sum)
+}
