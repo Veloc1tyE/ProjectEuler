f0a2a91c0b0e97fc7438081eccf177587d58bdfe
Will Jeremijenko <45901181+Veloc1tyE@users.noreply.github.com>
2019-10-24 21:42:46 +1000

    Update p1.swift

--- Swift/p1.swift
+++ Swift/p1.swift
@@ -1,1 +1,17 @@
+import Foundation
 
+func p1(_ n : Int) -> Int {
+    // Initialise total counter
+    var total = 0
+    /* Now loop through all values from 1 to n-1
+    And add to total if they are divisible by three or 5
+    */
+    for i in 1...n-1 {
+        if i % 3 == 0 || i % 5 == 0 {
+            total += i 
+        }
+    }
+    return total
+}
+
+print(p1(1000))
