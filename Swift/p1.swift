60304f94f15bb5345d05ed498a762614cf36cea0
Will Jeremijenko <45901181+Veloc1tyE@users.noreply.github.com>
2019-10-24 22:28:57 +1000

    Update p1.swift

--- Swift/p1.swift
+++ Swift/p1.swift
@@ -1,17 +1,32 @@
 import Foundation
 
-func p1(_ n : Int) -> Int {
-    // Initialise total counter
-    var total = 0
-    /* Now loop through all values from 1 to n-1
-    And add to total if they are divisible by three or 5
+/*
+Use of arithmetic series formula to solve problem quickly for hackerrank
+*/
+
+func p1(_ n : Int) -> Int {    
+    /* 
+    Check up to n-1, to find all divisible numbers
+    Express answer in terms of arithmetic sequences
     */
-    for i in 1...n-1 {
-        if i % 3 == 0 || i % 5 == 0 {
-            total += i 
-        }
-    }
-    return total
+    // First find sum of threes
+    let threeCap = (n - 1) / 3 
+    let threeSum = 3 * (threeCap * (threeCap + 1)) / 2
+    // Then sum of fives
+    let fiveCap = (n - 1) / 5
+    let fiveSum = 5 * (fiveCap * (fiveCap + 1)) / 2
+    // Then fifteens
+    let fifteenCap = (n - 1) / 15
+    let fifteenSum = 15 * (fifteenCap * (fifteenCap + 1)) / 2
+    // Add sums of numbers divisible by three or five below the cap
+    // Subtract the sum of numbers divisible by fifteen, these are counted twice
+    return threeSum + fiveSum - fifteenSum
 }
 
-print(p1(1000))
+// Hackerrank solution
+let tests = Int(readLine()!)
+
+for _ in 1...tests! {
+    let limit = Int(readLine()!)
+    print(p1(limit!))
+}
